31fd1711ee65630b7ec9f1c4e498f949051df51d
PatricioMaturana <51171849+PatricioMaturana@users.noreply.github.com>
2024-07-14 03:27:47 -0400

    Se agregan cambios/ mejoras al metodo productos

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,15 +1,64 @@
-const express = require("express")
-const router = express.Router()
-const products=[]
+const express = require("express");
+const router = express.Router();
+const fs = require('fs');
+const path = require("path");
+const products=[];
 
 router.get("/products",(req,res)=>{
-    res.json(products)
-})
-
+    if (fs.existsSync('productos.json') )
+    {
+        fs.readFile('productos.json','utf8',(err,data)=>{
+            if(err)
+            {
+                console.error(err);
+                return res.status(500).json({error:'Error servidor interno'});
+            }else{
+                const products = JSON.parse(data);
+                res.json(products);
+            }    
+        });
+    };
+    return res.status(204).json({error:'No hay datos'});
+});
 router.post("/products",(req,res)=>{
-    const newProducto = req.body
-    products.push(newProducto)
-    res.json({message:"Producto agregado"})
+    const {id, title, description, price, status, stock, category }= req.body;
+    if (fs.existsSync('productos.json') )
+    {
+        fs.readFile('productos.json','utf8',(err,data)=>{
+            if(err)
+            {
+                  console.error(err);
+                return res.status(500).json({error:'Error servidor interno'});
+            }else
+            {
+                const contenidoJson = JSON.parse(data);
+                const producto = contenidoJson.find(producto => producto.id == id) 
+                if (producto)
+                {
+                    return res.status(404).json({mensage:`Producto ya existe`});
+                }
+                products.push({ id, title, description, price, status, stock, category });
+                fs.writeFile('productos.json', JSON.stringify(products,null,2),err =>{
+                if(err){
+                  console.error(err);
+                  return res.status(500).json({error: 'Error Interno'});
+                }  
+                res.json(products);
+                });
+            }
+            
+        })
+    }else
+    {
+        products.push({ id, title, description, price, status, stock, category });
+        fs.writeFile('productos.json', JSON.stringify(products,null,2),err =>{
+            if(err){
+                  console.error(err);
+                  return res.status(500).json({error: 'Error Interno'});
+            }  
+            res.json(products);
+          });
+    }
 })
 
 module.exports = router;
