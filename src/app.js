4d9cc36a3bb65b0a3222805ba5e9ac614fbecead
PatricioMaturana <51171849+PatricioMaturana@users.noreply.github.com>
2024-07-15 00:42:40 -0400

    Se agregan cambios/ mejoras al metodo carts

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const productsRouter = require("./routes/api/products.js")
-//const cartsRouter    = require("./routes/api/carts.js")
+const cartsRouter    = require("./routes/api/carts.js")
 
 const app = express()
 const PORT = 8080
@@ -10,7 +10,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
 app.use("/",productsRouter)
-//app.use("/",cartsRouter)
+app.use("/",cartsRouter)
 
 app.listen(PORT,()=>{
     console.log(`Server running on port ${PORT}`)
--- src/routes/api/carts.js
+++ src/routes/api/carts.js
@@ -1,5 +1,7 @@
 const express = require("express")
 const router = express.Router()
+const fs = require('fs');
+
 const carts=[]
 
 router.get("/api/carts",(req,res)=>{
@@ -7,9 +9,47 @@ router.get("/api/carts",(req,res)=>{
 })
 
 router.post("/api/carts",(req,res)=>{
-    const newCarts = req.body
-    products.push(newCarts)
-    res.json({message:"Producto agregado al Carro"})
+    const {id, products} = req.body;
+    if(id && products){
+
+        if (fs.existsSync('carts.json') )
+            {
+                fs.readFile('carts.json','utf8',(err,data)=>{
+                    if(err)
+                    {
+                        console.error(err);
+                        return res.status(500).json({error:'Error servidor interno'});
+                    }else
+                    {
+                        const contenidoJson = JSON.parse(data);
+                        const newId = contenidoJson.length > 0 ? Math.max(...(contenidoJson.map(p => p.id))) + 1 : 1;
+                        console.log(newId)
+                        carts.push({ id:newId, products });
+                        fs.writeFile('carts.json', JSON.stringify(carts,null,2),err =>{
+                        if(err){
+                          console.error(err);
+                          return res.status(500).json({error: 'Error Interno'});
+                        }  
+                        res.json(carts);
+                        });
+                    }
+                    
+                })
+            }else
+            {
+                carts.push({ id:1, products });
+                fs.writeFile('carts.json', JSON.stringify(carts,null,2),err =>{
+                    if(err){
+                          console.error(err);
+                          return res.status(500).json({error: 'Error Interno'});
+                    }  
+                    res.json(carts);
+                  });
+            }
+
+
+
+    }
 })
 
 module.exports = router;
